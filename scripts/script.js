ae27e9ebacdcacd1a1f4b84582b7a5c0a2a393e8
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-09-23 09:51:34 +0300

    Commit 3

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,34 @@ contentField.style.width = screenWidth + 'px';
 // contentField.style.marginTop = 0 + 'px';
 contentField.style.margin = 'auto';
 
+// textures array --------------------------------------------
+
+let texturesArr = [
+  {sky: 'assets/sky.jpeg'}, 
+  {wall: 'assets/wall.jpg'},
+  {front: 'assets/view_.jpg'},
+  {floor: 'assets/floor.jpg'},
+];
+
+const corridor = document.querySelector('.corridor');
+
+const topBlock = document.getElementById('top-block');
+const leftBlock = document.getElementById('left-block');
+const frontBlock = document.getElementById('front-block');
+const rightBlock = document.getElementById('right-block');
+const downBlock = document.getElementById('down-block');
+
+function makeView(target, path) {
+  const img = document.createElement('img');
+  img.src = `${path}`;
+  target.append(img);
+}
+
+makeView(topBlock, texturesArr[0].sky);
+makeView(leftBlock, texturesArr[1].wall);
+makeView(frontBlock, texturesArr[2].front);
+makeView(rightBlock, texturesArr[1].wall);
+makeView(downBlock, texturesArr[3].floor);
 
 
 
@@ -16,14 +44,10 @@ contentField.style.margin = 'auto';
 
 
 
-
-
-
-
-// select disble -----------------------------------------
+// select disable -----------------------------------------
 
 document.addEventListener('selectstart', function (e) {
-    e.preventDefault(); // Відмінити подію виділення тексту
+    e.preventDefault(); 
   });
 
 
